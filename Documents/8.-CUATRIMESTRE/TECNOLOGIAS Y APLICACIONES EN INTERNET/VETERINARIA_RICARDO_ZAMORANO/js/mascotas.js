438f1662c9f12b5d5dfdc50dcdac5eb9d2fa913d
RICARDO URIEL ZAMORANO RIOS <uriel_zamorano95@hotmail.com>
2021-03-06 00:50:44 -0600

    segundo commint

--- Documents/8.-CUATRIMESTRE/TECNOLOGIAS Y APLICACIONES EN INTERNET/VETERINARIA_RICARDO_ZAMORANO/js/mascotas.js
+++ Documents/8.-CUATRIMESTRE/TECNOLOGIAS Y APLICACIONES EN INTERNET/VETERINARIA_RICARDO_ZAMORANO/js/mascotas.js
@@ -1,95 +1,106 @@
-const listaMascotas=document.getElementById('lista-mascotas');
-const tipo=document.getElementById('tipo');
-const nombre=document.getElementById('nombre');
-const dueno=document.getElementById('dueno');
-const indice=document.getElementById('indice');
-const btnGuardar=document.getElementById('btn-guardar');
-const form=document.getElementById('form');
+const listaMascotas = document.getElementById('lista-mascotas');
+const tipo = document.getElementById('tipo');
+const nombre = document.getElementById('nombre');
+const dueno = document.getElementById('dueno');
+const indice = document.getElementById('indice');
+const form = document.getElementById('form');
+const btnGuardar = document.getElementById('btn-guardar');
 
+let mascotas = [
+    {
+        tipo: "Perro",
+        nombre: "Chester",
+        dueno:"Ricardo"
+    },
 
-let mascotas=[ 
     {
         tipo: "Perro",
         nombre: "Firulais",
-        dueno:"Blanca"
-    },
-    {
-        tipo: "Gato",
-        nombre: "Misifu",
-        dueno:"Laura"
+        dueno:"Luis"
     }
-];
 
+    ];
 
-function listarMascotas()
-{
-    const htmlMascotas = mascotas.map((mascota, index)=>`<tr>
-    <th scope="row">${index}</th>
-    <td>${mascota.tipo}</td>
-    <td>${mascota.nombre}</td>
-    <td>${mascota.dueno}</td>
-    <td>
-        <div class="btn-group" role="group" aria-label="Basic example">
-            <button type="button" class="btn btn-info editar" ><i class="fas fa-edit"></i></button>
-            <button type="button" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
-        </div>
-    </td>
-  </tr>`).join("");
-listaMascotas.innerHTML=htmlMascotas;
-Array.from(document.getElementsByClassName('editar')).forEach((botonEditar, index)=>botonEditar.onclick=editar(index))
+function listarMascotas() {
+    const htmlMascotas = mascotas.map((mascota, index)=>`
+    <tr>
+                    <th scope="row">${index}</th>
+                    <td>${mascota.tipo}</td>
+                    <td>${mascota.nombre}</td>
+                    <td>${mascota.dueno}</td>
+                    <td>
+                        <div class="btn-group" role="group" aria-label="Basic example">
+                            <button type="button" class="btn btn-info editar"><i class="fas fa-edit"></i></button>
+                            <button type="button" class="btn btn-danger eliminar"><i class="fas fa-trash-alt"></i></button>
+                            
+                        </div>
+                    </td>
+                </tr>`).join("");
+    listaMascotas.innerHTML = htmlMascotas;
+    Array.from(document.getElementsByClassName('editar')).forEach((botonEditar, index)=>botonEditar.onclick = editar(index))
+    Array.from(document.getElementsByClassName('eliminar')).forEach((botonEliminar, index)=>botonEliminar.onclick = eliminar(index))
 }
 
-function enviarDatos(evento)
-{
+
+function enviarDatos(evento) {
     evento.preventDefault();
-    const datos =
-    {
+
+    const datos = {
         tipo: tipo.value,
         nombre: nombre.value,
         dueno: dueno.value
-    };
-    const accion=btnGuardar.innerHTML;
-    switch(accion)
-    {
+    
+        };
+    
+    const accion = btnGuardar.innerHTML;
+    switch(accion) {
         case 'Editar':
-        //editar
-        mascotas[indice.value]=datos;
-        break;
-
+            mascotas[indice.value] = datos;
+            break;
         default:
-            //crear
             mascotas.push(datos);
             break;
     }
     
+    
+    
     listarMascotas();
     resetModal();
-
 }
 
-function editar(index)
-{
-    return function cuandoCliqueo()
-    {
+function editar (index) {
+    
+    return function CuandoHagoClick() {
         btnGuardar.innerHTML='Editar'
         $('#exampleModalCenter').modal('toggle');
-        const mascota=mascotas[index];
-        nombre.value=mascota.nombre;
-        dueno.value=mascota.dueno;
-        tipo.value=mascota.tipo;
-        indice.value=index;
-    } 
+        const mascota = mascotas[index];
+        nombre.value = mascota.nombre;
+        dueno.value = mascota.dueno;
+        tipo.value = mascota.tipo;
+        indice.value = index;
+        
+    }
 }
 
-function resetModal()
-{
-    nombre.value='';
-    dueno.value='';
-    tipo.value='';
-    indice.value='';
-    btnGuardar.innerHTML='Crear'
+
+function resetModal() {
+    nombre.value = '';
+    dueno.value = '';
+    tipo.value = '';
+    indice.value = '';
+    btnGuardar.innerHTML='Guardar';
+}
+
+function eliminar(index) {
+    return function clickEliminar() {
+        
+        mascotas = mascotas.filter((mascota, indiceMascota)=> indiceMascota !== index);
+        listarMascotas();
+    }
+    
 }
 
 listarMascotas();
-form.onsubmit=enviarDatos;
-btnGuardar.onclick=enviarDatos;
+
+form.onsubmit = enviarDatos;
+btnGuardar.onclick = enviarDatos;
